a25792131e2ece68b5eebd64530621140887b29d
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-02-25 22:35:52 +0100

    Revert "export every type declaration"
    This reverts commit 8f1d1bb6d6c13ae3d5514b641983ed291838fb86.

--- app/src/lib/attatchToProto.ts
+++ app/src/lib/attatchToProto.ts
@@ -1,8 +1,6 @@
 import { constructApplyToPrototype } from "attatch-to-prototype"
 
-const defaultOptions = { enumerable: true }
-
 // apply (to) Target
-export const at = constructApplyToPrototype(EventTarget.prototype, defaultOptions)
+export const at = constructApplyToPrototype(EventTarget.prototype)
 // apply (to) Element
-export const ae = constructApplyToPrototype(Element.prototype, defaultOptions)
+export const ae = constructApplyToPrototype(Element.prototype)
