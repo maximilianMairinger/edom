8f1d1bb6d6c13ae3d5514b641983ed291838fb86
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-02-25 22:32:50 +0100

    export every type declaration

--- app/src/lib/attatchToProto.ts
+++ app/src/lib/attatchToProto.ts
@@ -1,6 +1,8 @@
 import { constructApplyToPrototype } from "attatch-to-prototype"
 
+const defaultOptions = { enumerable: true }
+
 // apply (to) Target
-export const at = constructApplyToPrototype(EventTarget.prototype)
+export const at = constructApplyToPrototype(EventTarget.prototype, defaultOptions)
 // apply (to) Element
-export const ae = constructApplyToPrototype(Element.prototype)
+export const ae = constructApplyToPrototype(Element.prototype, defaultOptions)
