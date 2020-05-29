ef1a2caa0f3ecb5c195eb622371cdd2ffea7244c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-29 21:10:19 +0200

    test

--- test/src/test.ts
+++ test/src/test.ts
@@ -1,33 +1,29 @@
 import init, { ElementList } from "../../app/src/edom"
-import { Data } from "josm"
-import tweenSvgPath from "tween-svg-path"
+import { DataBase } from "josm"
+import animationFrameDelta from "animation-frame-delta"
 
-let from = "path('M105 55C105 85.3757 85.3757 110 55 110C24.6243 110 0 85.3757 0 55C0 24.6243 24.6243 0 55 0C85.3757 0 105 24.6243 105 55Z')"
-let to = "M103.5 55.5C103.5 66 60.5 117 52.5 117C44.5 117 3.05176e-05 69 0 55.5C-3.05175e-05 42 27 -51.5 52.5 42C68.5 -47 103.5 36.4599 103.5 55.5Z"
-
-console.log("init");
-
-let scrollData = new Data(2)
 
 const mainElem = document.querySelector("#main")
 
+
+
 init().then(() => {
-  const elem = document.querySelector("#test") as HTMLElement
-  let d = new Data("hello")
-  
-  
-  elem.text(d)
+  let e = new DataBase({ok: "1okok1"})
+
+
+
+
+  mainElem.html(`<qwe style="color: white" whatEver="$[ok]" ok="okokokoko">ww$[ok]ww<span>/<qwe</span></qwe>`, e)
+
   setTimeout(() => {
-    d.set("ok")
-    elem.anim({width: 300})
+    let ok = e.ok
+    animationFrameDelta((q, e) => {
+      ok.set((Math.round(e)).toString())
+    })
   }, 1000)
-
-  let w = new ElementList(elem, mainElem)
-
-  let q = w.css("background")
-  console.log(q)
-
 })
+
+
 
 
 
