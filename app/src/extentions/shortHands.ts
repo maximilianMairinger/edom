7827886663a5bff451b501680eed47e80907d016
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-30 20:53:20 +0200

    class shorthands

--- app/src/extentions/shortHands.ts
+++ app/src/extentions/shortHands.ts
@@ -62,3 +62,29 @@ an("parent", function() {
   return this.parentElement
 })
 
+
+at("addClass", function(...className: string[]) {
+  this.classList.add(...className);
+  return this;
+})
+
+at("removeClass", function(...className: string[]) {
+  this.classList.remove(...className);
+  return this;
+})
+
+at("hasClass", function(...className: string[]) {
+  let has = true;
+  className.ea((cls) => {
+    if (!this.classList.contains(cls)) has = false;
+  });
+  return has
+})
+
+at("toggleClass", function(...className: string[]) {
+  className.ea((cls) => {
+    if (this.hasClass(cls)) this.removeClass(cls);
+    else this.addClass(cls);
+  });
+  return this
+})
