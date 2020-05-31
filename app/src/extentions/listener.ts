575f2e3aec61e56d6b17d97a2c8a90ce925558da
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-05-31 23:04:18 +0200

    deprecate listener

--- app/src/extentions/listener.ts
+++ app/src/extentions/listener.ts
@@ -1,6 +1,7 @@
 import { at } from "../lib/attatchToProto"
-import { Tel } from "../edom"
+import { Tel } from "./../components/tel"
 
 at(["listener", "listen", "ls"], function(event?: any, listener?: any, patch?: boolean) {
+  console.warn("EventTarget#listener is deprecated and will be removed in edom@4.0.0. Please migrate to EventTarget#on.")
   return new Tel(this, event, listener, patch)
 })
