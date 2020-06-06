4c2bf2e1d3bb8877f34ceeb703273fcb22d87967
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-06 13:33:29 +0200

    eventListener multiple targets / events / listener store

--- app/src/components/eventListener.ts
+++ app/src/components/eventListener.ts
@@ -4,26 +4,30 @@ import { internalIsSubscribed, internalOn, internalOff } from "../extentions/onO
 
 export const dataSubscriptionCbBridge = Symbol()
 
+type Listener<Event extends keyof EdomElementEventMap> = (this: EventTarget, ev: EdomElementEventMap[Event]) => void
+
 
 export class EventListener<Event extends keyof EdomElementEventMap = any, Options extends {[key in string]: any} = {[key in string]: any}> {
   private n: NS<Event>;
-  constructor(target: EventTarget, event?: Event, listener?: (this: EventTarget | Window, ev: EdomElementEventMap[Event]) => any, activate: boolean = true, options?: Options) {
-    this.n = new NS(target, event, listener, options);
+  private _options: any
+  constructor(target?: EventTarget | EventTarget[], event?: Event | Event[], listener?: Listener<Event> | Listener<Event>[], activate: boolean = true, options?: Options) {
+    this.n = new NS(target, event, listener);
+    if (options) this._options = options
     listener[dataSubscriptionCbBridge] = this
     if (activate) this.activate();
   }
 
-  public event(): Event
-  public event(event: Event): this
-  public event(event?: Event) {
+  public event(): Event[]
+  public event(event: Event | Event[]): this
+  public event(event?: Event | Event[]) {
     if (event !== undefined) {
       let active = this.active()
       if (active) this.deactivate();
-      this.n.event = event;
+      this.n.event(event)
       if (active) this.activate();
       return this
     }
-    else return this.n.event;
+    else return this.n.event();
   }
 
   public options(): Options
@@ -32,43 +36,42 @@ export class EventListener<Event extends keyof EdomElementEventMap = any, Option
     if (options !== undefined) {
       let active = this.active()
       if (active) this.deactivate();
-      this.n.options = options;
+      this._options = options;
       if (active) this.activate();
       return this
     }
-    else return this.n.options;
+    else return this._options;
   }
 
-  public target(): EventTarget
-  public target(target: EventTarget): this
-  public target(target?: EventTarget) {
+  public target(): EventTarget[]
+  public target(target: EventTarget[] | EventTarget): this
+  public target(target?: EventTarget[] | EventTarget) {
     if (target !== undefined) {
       let active = this.active()
       if (active) this.deactivate();
-      this.n.target = target
+      this.n.target(target)
       if (active) this.activate();
       return this
     }
-    else return this.n.target;
+    else return this.n.target();
   }
 
 
-  public listener(): (this: EventTarget, ev: EdomElementEventMap[Event]) => void
-  public listener(listener: (this: EventTarget, ev: EdomElementEventMap[Event]) => void): this
-  public listener(listener?: (this: EventTarget, ev: EdomElementEventMap[Event]) => void) {
+  public listener(): Listener<Event>[]
+  public listener(listener: Listener<Event> | Listener<Event>[]): this
+  public listener(listener?: Listener<Event> | Listener<Event>[]) {
     if (listener) {
       if (listener !== this.n.listener) {
         let active = this.active()
         if (active) this.deactivate();
-        delete this.n.listener[dataSubscriptionCbBridge]
-        this.n.listener = listener;
+        this.n.listener(listener)
         listener[dataSubscriptionCbBridge] = this
         if (active) this.activate();
       }
       
       return this
     }
-    else return this.n.listener;
+    else return this.n.listener();
     
   }
 
@@ -97,8 +100,36 @@ export class EventListener<Event extends keyof EdomElementEventMap = any, Option
   }
 }
 
-class NS<K extends keyof EdomElementEventMap = any> {
-  constructor(public target: EventTarget, public event: K, public listener: (this: EventTarget, ev: EdomElementEventMap[K]) => void, public options: any) {
-
+class NS<Event extends keyof EdomElementEventMap = any> {
+  private _target: EventTarget[]
+  private _event: Event[]
+  private _listener: Listener<Event>[]
+  constructor(target: EventTarget[] | EventTarget, event: Event[] | Event, listener: Listener<Event>[] | Listener<Event>) {
+    this.target(target)
+    this.event(event)
+    this.listener(listener)
   }
+
+  public target(): EventTarget[]
+  public target(target: EventTarget[] | EventTarget): void
+  public target(target?: EventTarget[] | EventTarget): any {
+    if (target) this._target = this.arrayify(target)
+    else return this._target
+  }
+  public event(): Event[]
+  public event(event: Event[] | Event): void
+  public event(event?: Event[] | Event): any {
+    if (event) this._event = this.arrayify(event)
+    else return this._event
+  }
+  public listener(): Listener<Event>[]
+  public listener(listener: Listener<Event>[] | Listener<Event>): void
+  public listener(listener?: Listener<Event>[] | Listener<Event>): any {
+    if (listener) this._listener = this.arrayify(listener)
+    else return this._listener
+  }
+  private arrayify<T>(q: T): T extends any[] ? T : [T] {
+    return (q instanceof Array ? q : [q]).clean() as any
+  }
+
 }
