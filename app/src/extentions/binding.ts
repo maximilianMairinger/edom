02f5342cfd360e18fa8bc7585d797a17705166a3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-07 19:22:33 +0200

    scroll binding implementation

---
+++ app/src/extentions/binding.ts
@@ -0,0 +1,30 @@
+import { et } from "../lib/attatchToProto";
+import { constructIndexStore } from "../lib/indexing";
+import { Data } from "josm";
+
+const getScrollData = constructIndexStore((elem: Element) => {
+  const data = new Data(elem.scrollTop)
+  elem.on("scroll", () => {
+    data.set(elem.scrollTop)
+  })
+  return data
+})
+
+et("scrollData", function() {
+  return getScrollData(this)
+})
+
+et("scrollEvent", function(at: number, listenerForward: () => void, listenerBack: () => void, margin = 0) {
+  const data = getScrollData(this)
+  margin = margin / 2
+  const atForward = at + margin
+  let atBack = at - margin
+  if (atBack < 0) atBack = 0
+  let lastProg = 0
+  let sub = data.get((prog) => {
+    if (prog >= atForward && lastProg < atForward) listenerForward()
+    else if (prog < atBack && lastProg >= atBack) listenerBack()
+  })
+  return sub
+})
+
