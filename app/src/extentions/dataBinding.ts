d9fbcb6f28307d2e2f352c834d11235ebdd8661d
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-26 21:10:10 +0200

    scroll events proper data implementation

--- app/src/extentions/dataBinding.ts
+++ app/src/extentions/dataBinding.ts
@@ -1,17 +1,57 @@
 import { et } from "../lib/attatchToProto"
 import { constructIndex } from "key-index"
-import { Data } from "josm"
+import { Data, DataSubscription } from "josm"
+
+export class ScrollData extends Data<number> {
+  constructor(initialScrollProgress = 0) {
+    super(initialScrollProgress)
+  }
+  scrollTrigger(at: number, margin?: number) {
+    return new ScrollTrigger(this, at, margin)
+  }
+}
+
+export class ScrollTrigger {
+  private listener: {
+    forward: ((currentPos: number) => void)[],
+    backward: ((currentPos: number) => void)[]
+  } = {
+    forward: [],
+    backward: []
+  }
+  public subscription: DataSubscription<[number]>
+  constructor(scrollData: Data<number>, at: number, margin = 0) {
+    margin = margin / 2
+    const atForward = at + margin
+    let atBack = at - margin
+    if (atBack < 0) atBack = 0
+    let lastProg = 0
+    this.subscription = scrollData.get((prog) => {
+      if (prog >= atForward && lastProg < atForward) this.listener.forward.Call(prog)
+      else if (prog < atBack && lastProg >= atBack) this.listener.backward.Call(prog)
+      lastProg = prog
+    })
+  }
+  on(direction: "forward" | "backward", listener: (currentPos: number) => void) {
+    this.listener[direction].add(listener)
+    return this
+  }
+  off(direction: "forward" | "backward", listener: (currentPos: number) => void) {
+    this.listener[direction].rmV(listener)
+    return this
+  }
+}
 
 const getScrollData = constructIndex((elem: HTMLElement | Window) => {
-  let data: Data<number>
+  let data: ScrollData
   if (elem instanceof Window) {
-    data = new Data(elem.scrollY)
+    data = new ScrollData(elem.scrollY)
     elem.on("scroll", () => {
       data.set(elem.scrollY)
     })
   }
   else {
-    data = new Data(elem.scrollTop)
+    data = new ScrollData(elem.scrollTop)
     elem.on("scroll", () => {
       data.set(elem.scrollTop)
     })
@@ -25,17 +65,6 @@ et("scrollData", function() {
 })
 
 et(["scrollEvent", "scrollTrigger"], function(at: number, listenerForward: () => void, listenerBack: () => void, margin = 0) {
-  const data = getScrollData(this)
-  margin = margin / 2
-  const atForward = at + margin
-  let atBack = at - margin
-  if (atBack < 0) atBack = 0
-  let lastProg = 0
-  let sub = data.get((prog) => {
-    if (prog >= atForward && lastProg < atForward) listenerForward()
-    else if (prog < atBack && lastProg >= atBack) listenerBack()
-    lastProg = prog
-  })
-  return sub
+  (this.scrollData() as ScrollData).scrollTrigger(at, margin).on("forward", listenerForward).on("backward", listenerBack)
 })
 
