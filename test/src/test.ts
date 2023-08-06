59b054668847e1760284f23e9b07448952dd36c0
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-08-06 13:59:03 +0200

    some tests

--- test/src/test.ts
+++ test/src/test.ts
@@ -3,19 +3,31 @@ import init, { ElementList, EventListener } from "../../app/src/edom"
 
 init().then(() => {
   let el = document.childs("#txt")
-  el.css({opacity: 0})
-  const dat = new Data("wooo")
-
-  el.innerText = dat.get()
-
-  const elC = el.childNodes[0]
-
-  elC.txt(dat, false, true)
+  el.txt("lel")
+  const anim = el.animate([
+    {opacity: '1', offset: 0},
+    {opacity: '0.5', offset: 1}
+  ], {fill: 'both', iterations: 1, duration: 400, easing: 'ease'})
 
 
-  setTimeout(() => {
-    dat.set("wooo2")
-  }, 1000)
+  console.log(anim)
+  anim.onfinish = () => {
+    console.log("done test2")
+  }
+
+
+  // const dat = new Data("wooo")
+
+  // el.innerText = dat.get()
+
+  // const elC = el.childNodes[0]
+
+  // elC.txt(dat, false, true)
+
+
+  // setTimeout(() => {
+  //   dat.set("wooo2")
+  // }, 1000)
 
 
 
